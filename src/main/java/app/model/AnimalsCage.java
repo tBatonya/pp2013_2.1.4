1650aaff049123650a26d51e5bdec86ef7776b48
Tonya Yurina <tonyabatonya@yandex.ru>
2023-08-24 00:28:47 +0300

    add class dog and connect with AnimalCage

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,21 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AnimalsCage {
+    private final Animal animal;
+    private final Timer timer;
+    public AnimalsCage(@Qualifier("dog")Animal animal, Timer timer){
+        this.animal = animal;
+        this.timer = timer;
+    }
 
-    @Autowired
-    private Animal animal;
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
